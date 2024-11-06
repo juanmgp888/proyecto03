e80c755bc8837bddfc7dfd338e83598a983025be
curso04 <curso04@gmail.com>
2024-11-06 10:25:50 +0100

    He puesto constructor fuera de cada método. Compartido por todo ellos.

--- src/test/java/es/santander/ascender/proyecto03/CalculadoraTest.java
+++ src/test/java/es/santander/ascender/proyecto03/CalculadoraTest.java
@@ -14,44 +14,32 @@ public class CalculadoraTest {
     /**
      Calculadora de números enteros
      */
+    // llamada al constructor
+    Calculadora calculadora = new Calculadora();
+
     @Test
     // métodos con el prefijo test. @test antecediendo a cada método que vayamos a testear
-
-
     public void testSumar() {
-        // llamada al constructor
-        Calculadora calculadora = new Calculadora();
 
         long resultado = calculadora.sumar(8,3);
         assertEquals(11,resultado);
-
     }
 
     @Test
     public void testRestar() {
-        // llamada al constructor
-        Calculadora calculadora = new Calculadora();
-
         long resultado = calculadora.restar(8,3);
         assertEquals(5,resultado);
-;
     }
+
     @Test
     public void testMultiplicar() {
-        // llamada al constructor
-        Calculadora calculadora = new Calculadora();
-
         long resultado = calculadora.multiplicar(8,3);
         assertEquals(24,resultado);
-;
     }
+
     @Test
     public void testDividir() {
-        // llamada al constructor
-        Calculadora calculadora = new Calculadora();
-
         long resultado = calculadora.dividir(8,3);
         assertEquals(2,resultado);
-;
     }
 }
